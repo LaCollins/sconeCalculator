76f2aec277368f73337d278ac8478059cd9cde64
Laura Collins <laura.e.collins1@gmail.com>
2019-08-17 16:30:18 -0500

    adding Javascript variables

---
+++ code.js
@@ -0,0 +1,15 @@
+const g = ' grams';
+const cream = 'Cream: ';
+const mix = 'Mix: ';
+const blue = 'Blueberries: ';
+const cinn = 'Cinnamon Chips: ';
+const or = 'Orange Zest: ';
+const ap = 'Apple Filling: ';
+
+var heavyCreaLS;
+var heavyCreamSS;
+var mixLS;
+var mixSS;
+var additiveLS;
+var additiveSS;
+
