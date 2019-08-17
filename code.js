968bf9b1552dc8d08672939e4789a9ffac447cad
Laura Collins <laura.e.collins1@gmail.com>
2019-08-17 17:28:50 -0500

    added more js

--- code.js
+++ code.js
@@ -7,15 +7,39 @@ const or = 'Orange Zest: ';
 const ap = 'Apple Filling: ';
 
 let largeSconeQuantity;
+let smallSconeQuantity;
 
-let heavyCreaLS;
+let heavyCreamLS;
 let heavyCreamSS;
 let mixLS;
 let mixSS;
 let additiveLS;
 let additiveSS;
 
-function recipe() {
+let totalCream;
+let totalMix;
+let totalAdd;
+
+function recipe(sconeType) {
     largeSconeQuantity =  document.getElementById('largeScone').value;
-    console.log(largeSconeQuantity);
+    smallSconeQuantity = document.getElementById('smallScone').value;
+
+    heavyCreamLS = [37];
+    heavyCreamSS = [60];
+    mixLS = [53];
+    mixSS = [85];
+    additiveLS = [22];
+    additiveSS = [35];
+
+    if (sconeType === 'blueberry') {
+
+    totalCream = ((largeSconeQuantity * heavyCreamLS[0]) + (smallSconeQuantity * heavyCreamSS[0]));
+    document.getElementById('cream').innerHTML = cream + totalCream + g;
+    
+    totalMix = ((largeSconeQuantity * mixLS[0]) + (smallSconeQuantity * mixSS[0]));
+    document.getElementById('mix').innerHTML = mix + totalMix + g;
+
+    totalAdd = ((largeSconeQuantity * additiveLS[0]) + (smallSconeQuantity * additiveSS[0]));
+    document.getElementById('ingredient').innerHTML = blue + totalAdd + g;
+    }
 }
